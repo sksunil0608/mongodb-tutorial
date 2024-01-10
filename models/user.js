6cf991c6fd3de459157c25db99300095a30588c4
sksunil0608 <sksunil0608@gmail.com>
2024-01-10 15:44:25 +0530

    cart item delete functionality

--- models/user.js
+++ models/user.js
@@ -65,13 +65,47 @@ class User{
       );
 
   }
+
+  getCart(){
+    const db = getDb();
+    const productIds = this.cart.items.map(i=>{
+      return i.productId;
+    });
+    return db.collection('products')
+    .find({_id:{$in:productIds}})
+    .toArray().then(products=>{
+      return products.map(product=>{
+        return  {
+          ...product,
+          quantity:this.cart.items.find(i=>{
+            return i.productId.toString()=== product._id.toString()
+          }).quantity
+        };
+      });
+    });
+  }
+
+  deleteItemFromCart(productId){
+    const db = getDb();
+    // I can sepratly find that product id and update the cart item
+    // without that product id
+    const updatedCartItems = this.cart.items.filter(item=>{
+      return item.productId.toString() !== productId.toString();
+    });
+    return db.collection('users')
+    .updateOne(
+      {_id:new ObjectId(this._id)},
+      {$set:{cart: {items:updatedCartItems}}}
+    )
+  }
+
   static findById(userId){
-    const db = getDb()
+    const db = getDb();
     return db.collection('users')
     .find({_id:new ObjectId(userId)}).
     next().then(user=>{
       return user
-    }).catch(error=>{console.log(error)})
+    }).catch(error=>{console.log(error)});
   }
 }
 
