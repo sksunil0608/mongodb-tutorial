f1d8d92ce5a79127acd3563ce73414601c0fc948
sksunil0608 <sksunil0608@gmail.com>
2024-01-10 21:25:57 +0530

    realation between cart user and order, get oreder placed

--- models/user.js
+++ models/user.js
@@ -99,6 +99,36 @@ class User{
     )
   }
 
+  addOrder(){
+    const db = getDb();
+    return this.getCart().then(products=>{
+      const order = {
+        items: products,
+        user: { 
+          _id: new ObjectId(this._id),
+          name:this.name
+        }
+      }
+      return db.collection('orders').insertOne(order)
+    }).then(result=>{
+      this.cart = {items:[]}
+
+      return db.collection('users')
+        .updateOne(
+          { _id: new ObjectId(this._id) },
+          { $set: { cart: { items: [] } } }
+        )
+    })
+  }
+  getOrders(){
+    const db = getDb();
+    console.log(this._id)
+    return db.collection('orders').find({'user._id':new ObjectId(this._id)}).toArray()
+    .then(orders=>{
+      return orders
+    }).catch(error=>{console.log(error)})
+  }
+
   static findById(userId){
     const db = getDb();
     return db.collection('users')
